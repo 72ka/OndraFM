12409746ea78a9126b84d080141d618001dbcbc3
72ka <2hp@seznam.cz>
2026-03-24 16:19:45 +0100

    TASM and SJASMPlus compatibility
    Automatic assembly with TASM or SJASMPlus. No another actions needed.
    
    Updated README including info about RAM Test

--- README.md
+++ README.md
@@ -4,14 +4,12 @@ File manager used for loading programs into Ondra SPO 186 from SD card using Ond
 Based on Martin1's source codes, with some added functionalities:
 - joystick support
 - files and directories sorting  (using the "S" key)
+- RAM test
 
 ---
 
 Compile with *TASM* (version 3.2 tested)
 `tasm -80 -b make.asm __ondrafm.bin`
 
-To compile with *SjASMPlus*, make the following changes in *make.asm* file:  
-- comment all the `#define` lines at the file start  
-- replace # in all `#include` lines with spaces (or any non-zero number of whitespace characters)  
-
-`sjasmplus --raw=__ondrafm.bin make.asm`
+To compile with *SjASMPlus* (version v1.21.0 tested)  
+`sjasmplus make.asm`
--- make.asm
+++ make.asm
@@ -1,13 +1,16 @@
-; pripraveno pro TASM 
-; pro kompilaci pres SjASMPlus nutno zakomentovat vsechna nasledujici #define 
-; a nahradit vsechna "#include" za " include" (mezera misto # na zacatku!)
+; pripraveno pro TASM i SJASMPlus
 
+; detekce SjASMPlus
+	IFDEF __SJASMPLUS__
+	OUTPUT "__ondrafm.bin"
+	ELSE
 #define 	EQU 	.equ
 #define 	END 	.end
 #define 	DB   	.DB
 #define 	DW   	.DW
 #define 	DM	.TEXT
 #define 	ORG	.org
+	ENDIF
 
 ; --------------------------------------------
 ; pstart urcuje zaciatok programu
@@ -31,17 +34,30 @@ pstart  EQU 9000h
                 ORG pstart
 ; --------------------------------------------
 ; includes
+
+	IFDEF __SJASMPLUS__
+	include "main.asm"
+	include "keyboard.asm"
+	include "mainprn.asm"
+	include "hexaprn.asm"
+	include "font6x8f.asm"
+	include "loader.asm"
+	include "txd.asm"
+	include "beep.asm"
+	include "memtest.asm"
+	include "sort.asm"
+	ELSE
 #include "main.asm"
 #include "keyboard.asm"
 #include "mainprn.asm"
 #include "hexaprn.asm"
 #include "font6x8f.asm"
-;#include "easter.asm"
 #include "loader.asm"
 #include "txd.asm"
 #include "beep.asm"
 #include "memtest.asm"
 #include "sort.asm"
+	ENDIF
 ;
 ; --------------------------------------------
 ; hlavicka s adresou sputenia kodu
